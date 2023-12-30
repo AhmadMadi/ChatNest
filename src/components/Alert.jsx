b4a4d37815cdd5908ec5fe26b035a9070bdaa408
Ahmad Madi <madi96.at.work@gmail.com>
2023-12-30 16:41:38 +0300

    Add Alert.jsx component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,16 @@
+import { forwardRef } from "react";
+import { Snackbar, Alert as MuiAlert } from "@mui/material";
+
+const Alert = (incomingProps) => {
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  return (
+    <Snackbar open={incomingProps.isOpen} autoHideDuration={4000}>
+      <Alert severity={incomingProps.type}>{incomingProps.message}</Alert>
+    </Snackbar>
+  );
+};
+
+export default Alert;
