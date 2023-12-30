25a785b36893b66890c8173d8c202da60ceb14fa
Ahmad Madi <madi96.at.work@gmail.com>
2023-12-30 15:22:30 +0300

    Update App.jsx and add App.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./App.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { Container } from "@mui/material";
+import Register from "./components/Register";
+
 function App() {
-  return <>ChatNest</>;
+  return (
+    <Container maxWidth="lg">
+      <Register />
+    </Container>
+  );
 }
 
 export default App;
