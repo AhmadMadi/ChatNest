c063694d97c304e71f17e1efefe6ba9d91cbc3f8
Ahmad Madi <madi96.at.work@gmail.com>
2023-12-31 14:44:22 +0300

    Implement UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,18 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { useState } from "react";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, errorCodeMapper } from "./config/firebase";
-
+import { useContext, useEffect, useState } from "react";
 import { Container } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import Register from "./components/Register";
-import Alert from "./components/Alert";
 import Chat from "./pages/Chat";
+import { UserContext } from "./context/UserContext";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [alertData, setAlertData] = useState({
-    isOpen: false,
-    type: "",
-    message: "",
-  });
-
-  const handleSubmit = async (e) => {
-    setIsLoading(true);
-
-    e.preventDefault();
-    const displayName = e.target.displayName.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-      await updateProfile(response.user, {
-        displayName,
-      });
-
-      setUser(response.user);
-    } catch (error) {
-      setAlertData({
-        isOpen: true,
-        type: "error",
-        message: errorCodeMapper(error.code),
-      });
-    }
-
-    setIsLoading(false);
-  };
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { user } = useContext(UserContext);
 
   const darkTheme = createTheme({
     palette: {
@@ -62,20 +23,17 @@ function App() {
     },
   });
 
+  useEffect(() => {
+    console.log("yoooo");
+    setIsLoggedIn(!!user);
+  }, [user]);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container maxWidth="lg">
-        {user ? (
-          <Chat />
-        ) : (
-          <Register handleSubmit={handleSubmit} isLoading={isLoading} />
-        )}
-        <Alert
-          isOpen={alertData.isOpen}
-          type={alertData.type}
-          message={alertData.message}
-        />
+        {/* {isLoggedIn ? <Chat /> : <Register />} */}
+        <Chat />
       </Container>
     </ThemeProvider>
   );
