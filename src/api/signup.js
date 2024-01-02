58342c7642434a3fd3204aff152963b24fd1fa86
Ahmad <ahmad.madi@washmen.com>
2024-01-02 21:53:37 +0300

    Add to user collection on signin

--- src/api/signup.js
+++ src/api/signup.js
@@ -1,12 +1,18 @@
-import { auth } from "../config/firebase";
+import { auth, db } from "../config/firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
 
 export const signup = async ({ displayName, email, password }) => {
-  console.log({ displayName, email, password });
   const { user } = await createUserWithEmailAndPassword(auth, email, password);
 
   await updateProfile(user, {
     displayName,
+  });
+
+  await addDoc(collection(db, "messages"), {
+    userId: user.uid,
+    displayName: user.displayName,
+    createdAt: new Date().getTime(),
   });
 
   return user;
