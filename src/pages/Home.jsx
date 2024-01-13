11d73ac9c1dd97eb23ab310eef7cb8fa73a50558
Ahmad Madi <madi96.at.work@gmail.com>
2024-01-13 17:43:43 +0300

    Remove unneeded import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import GoogleIcon from "@mui/icons-material/Google";
 
 import { Box, Stack, Typography, Checkbox } from "@mui/material";
